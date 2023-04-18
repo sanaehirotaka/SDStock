cb6d181f5060ba5b03e9e1a722e353c0b2d46137
sanaehirotaka <admin@sanaechan.net>
2023-04-18 21:24:28 +0900

    update

---
+++ SDStock/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows;
+
+namespace SDStock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await webView2.EnsureCoreWebView2Async(null);
+            webView2.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets.example", "assets", CoreWebView2HostResourceAccessKind.Allow);
+            webView2.CoreWebView2.Navigate("https://appassets.example/index.html");
+        }
+    }
+}
